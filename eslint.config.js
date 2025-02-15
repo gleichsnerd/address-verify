04fa0fe3135f2f12dd733d0aef493b2c21d49e73
River Gleichsner <gleichsnerd@gmail.com>
2025-02-15 17:32:04 -0500

    build: Adding full eslint and prettier configs

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,30 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import prettierConfig from 'eslint-config-prettier';
+import prettierPlugin from 'eslint-plugin-prettier';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: globals.browser }},
+  {
+    ignores: ['**/dist/**/*', '**/node_modules/**/*'],
+    files: ['**/*.{js,mjs,cjs,ts}'],
+  },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+    },
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  prettierConfig,
 ];
